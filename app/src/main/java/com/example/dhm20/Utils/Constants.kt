0007dbcb6e9641bd57c1e0922e853c973099bd2b
ri-dev-git <rishi9850@outloo.com>
2024-12-25 19:38:47 +0530

    fix

--- app/src/main/java/com/example/dhm20/Utils/Constants.kt
+++ app/src/main/java/com/example/dhm20/Utils/Constants.kt
@@ -1,3 +1,20 @@
 package com.example.dhm20.Utils
 
  const val CLIENT_ID="461259927203-cc39206qgtobopp21r3k0076644e06p5.apps.googleusercontent.com"
+
+val feelingsList = listOf(
+ "I've been feeling optimistic about the future",
+ "I've been feeling useful",
+ "I've been feeling relaxed",
+ "I've been feeling interested in other people",
+ "I've had energy to spare",
+ "I've been dealing with problems well",
+ "I've been thinking clearly",
+ "I've been feeling good about myself",
+ "I've been feeling close to other people",
+ "I've been feeling confident",
+ "I've been able to make up my own mind about things",
+ "I've been feeling loved",
+ "I've been interested in new things",
+ "I've been feeling cheerful"
+)
